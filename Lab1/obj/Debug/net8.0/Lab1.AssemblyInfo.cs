a8a25cf82f7cb038bb0fa916f642b68842314dae
sharky0377 <16onolt@gmail.com>
2025-07-25 02:05:41 +0000

    Created Lab4 console

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869f6220ed76b69a41edb409249f564d81dc6223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37d8a566ff0ca32a9e831a6951a4bc2a9b42a63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
