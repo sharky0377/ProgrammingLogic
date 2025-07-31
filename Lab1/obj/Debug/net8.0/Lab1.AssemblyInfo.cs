17cf92320c4c9c4685000127c20829e4dbe79d79
sharky0377 <16onolt@gmail.com>
2025-07-31 03:07:44 +0000

    Started and Finished Lab5

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a25cf82f7cb038bb0fa916f642b68842314dae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3dab3b4bd088b46f7062e3074bb4c32b5d2637")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab5/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Lab5;
+
+using System;
+
+// Step 1 - Create a Base Class
+class Animal
+{
+    public virtual void animalSound()
+    {
+        Console.WriteLine(" The animal makes a sound. ");
+    }
+}
+
+// Step 2 - Create Derived Classes
+
+// Cat class inherits from Animal
+class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine(" The cat says meow. ");
+    }
+}
+
+// Dog class inherits from Animal
+class Dog : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine(" The dog says: bow wow.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Demonstrating Runtime Polymorphism ---");
+
+        // Step 3: Test in Main()
+
+        Animal myAnimal = new Animal();
+        myAnimal.animalSound(); // Calls the base Animal's animalSound()
+
+        Animal myCat = new Cat();
+        myCat.animalSound(); // Calls Cat's overridden animalSound() at runtime
+
+        Animal myDog = new Dog();
+        myDog.animalSound(); // Calls Dog's overridden animalSound() at runtime
+
+        Console.WriteLine("\nThis demonstrates runetime polymorphism: " +
+                          "even though 'myCat' and 'myDog' are referenced as 'Animal' types, " +
+                          "the correct overridden method is called based on the actual object type at runtime.");
+    }
+}
